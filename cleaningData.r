0619f8c3645cf128b422ea5611025584d5250f8c
diegomarq <diegomarques.azevedo@gmail.com>
2017-07-04 01:20:39 -0300

    trying to do sentiment analysis with bag of words

--- cleaningData.r
+++ cleaningData.r
@@ -1,6 +1,8 @@
 install.packages("kernlab")
-library(kernlab)
 install.packages('readr')
+
+#### Upload and clean data
+library(kernlab)
 library(readr)
 
 # Upload data as object
@@ -11,65 +13,51 @@ dadosLinear <- read_delim("~/Documents/Training-Set-MDM/dadosLinear.csv",
 dadosLinear <- dadosLinear[1:1000,]
 
 # Object dimension
-dim(dadosLinear)
-
+#dim(dadosLinear)
 # Verify names
-names(dadosLinear)
-
+#names(dadosLinear)
 # Verify data types by collumn
-sapply(dadosLinear[1,], class)
-
+#sapply(dadosLinear[1,], class)
 # Last elements of the table
-tail(dadosLinear)
+#tail(dadosLinear)
 
 # Drop columns
 dadosLinear <- dadosLinear[,c("X3", "X5", "X8", "X9", "X11")]
-
 # Rename columns
 dadosLinear <- setNames(dadosLinear, c("date", "title", "vehicle", "category", "body"))
-
 # Verify rows with na value
 row_has_na <- apply(dadosLinear, 1, function(x){any(is.na(x))})
-
 # Number of rows with na
-sum(row_has_na)
-
+#sum(row_has_na)
 # Get data frame without na rows
 dadosLinear <- dadosLinear[!row_has_na,]
-
+row_has_na <- NULL
 # Verify NULL elements
 null_vector <- !grepl("NULL", dadosLinear$category, ignore.case = TRUE)
-length(null_vector)
-
+#length(null_vector)
 # Select data frame without null element in category row
 dadosLinear<- dadosLinear[null_vector,]
-
+null_vector <- NULL
 # Count and remove duplicated rows
-sum(duplicated(dadosLinear))
+#sum(duplicated(dadosLinear))
 dadosLinear <- dadosLinear[!duplicated(dadosLinear),]
-
 # Remove quotes
 dadosLinear$title <- gsub("\"", "", dadosLinear$title)
 dadosLinear$body <- gsub("\"", "", dadosLinear$body)
 
 # Verify whether exists \n caracter in data
-install.packages("stringr")
-library(stringr)
-
-sum(grepl("\n", dadosLinear$title))
-sum(grepl("\n", dadosLinear$body))
-
-dadosLinear[grepl("[()]", dadosLinear$title),]
+#install.packages("stringr")
+#library(stringr)
+#sum(grepl("\n", dadosLinear$title))
+#sum(grepl("\n", dadosLinear$body))
+#dadosLinear[grepl("[()]", dadosLinear$title),]
 # Remove words between parentheses in title
 dadosLinear$title <- gsub("\\s*\\([^\\]+\\)", "", dadosLinear$title)
-
 # Verify words between parenthes in body
-grep("[()]", dadosLinear$body)
-
+#grep("[()]", dadosLinear$body)
 # Remove parentheses from body
 dadosLinear$body <- gsub("[(|)]", "", dadosLinear$body)
-
-dadosS <- dadosLinear
+#dadosS <- dadosLinear
 
 # Get last word
 # dadosS$vehicle <- str_extract(dadosS$vehicle, '\\w+$')
@@ -88,71 +76,73 @@ dadosS <- dadosLinear
 
 
 ######## Test word cloud
-
-library(tm)
 install.packages("wordcloud")
+library(tm)
 library(wordcloud)
 library(RColorBrewer)
-dadosTeste <- dadosLinear
 
 # Create corpus
-dadosCorpus <- Corpus(VectorSource(dadosTeste$body))
-
+dadosCorpus <- Corpus(VectorSource(dadosLinear$body))
 # Remove whitespace
 dadosCorpus <- tm_map(dadosCorpus, stripWhitespace)
-
 # Converto letter for lowercase
 dadosCorpus <- tm_map(dadosCorpus, tolower)
-
 # Remove punctuation
 dadosCorpus <- tm_map(dadosCorpus, removePunctuation)
-
 # Remove stopwords
 dadosCorpus <- tm_map(dadosCorpus, removeWords, stopwords("portuguese"))
-
 # Remove numbers
 dadosCorpus <- tm_map(dadosCorpus, removeNumbers)
-
+# stem Document
+dadosCorpus <- tm_map(dadosCorpus, stemDocument)
+  
+### Do stem?
+  
 # Term document matrix
-dadosTDMatrix <- TermDocumentMatrix(dadosCorpus)
+dadosTM <- DocumentTermMatrix(dadosCorpus)
+# Sparse terms
+dadosSparse <- removeSparseTerms(dadosTM, 0.99)
 
-# Matrix
-dadosMatrix <- as.matrix(dadosTDMatrix)
+# Convert to data frame
+dadosDF <- as.data.frame(as.matrix(dadosSparse))
+colnames(dadosDF) <- make.names(colnames(dadosDF))
+
+# Split into train and test
+dadosDFTrain <- head(dadosDF, nrow(dadosLinear[700,])) # 70%
+dadosDFTest <- head(dadosDF, nrow(dadosLinear[300,])) # 30%
+
+
+
 
 # Frequency
-dadosWordFreq <- sort(rowSums(dadosMatrix), decreasing = TRUE)
-
+#dadosWordFreq <- sort(rowSums(dadosMatrix), decreasing = TRUE)
 # Define limit by frequency
-dadosLimit <- quantile(dadosWordFreq, probs = 0.25)
-
+#dadosLimit <- quantile(dadosWordFreq, probs = 0.25)
 # Limit words
-dadosTopFreq <- dadosWordFreq[dadosWordFreq > dadosLimit]
-
+#dadosTopFreq <- dadosWordFreq[dadosWordFreq > dadosLimit]
 # Create Data Frame with words and its frequency
-dadosDataFrame <- data.frame(word = names(dadosTopFreq), freq = dadosTopFreq)
-
+#dadosDataFrame <- data.frame(word = names(dadosTopFreq), freq = dadosTopFreq)
 # plot wordcloud
-wordcloud(dadosDataFrame$word, dadosDataFrame$freq, random.order = FALSE, colors = brewer.pal(8, "Dark2"))
+#wordcloud(dadosDataFrame$word, dadosDataFrame$freq, random.order = FALSE, colors = brewer.pal(8, "Dark2"))
 
 
-### Start test of sentiment analysis
+### Sentiment analysis
 
 # Install dependencies
 install.packages("tm")
 install.packages("RTextTools")
 library("RTextTools")
-install.packages("caret")
-library("caret")
+#install.packages("caret")
+#library("caret")
+#install.packages("tidytext")
+#library(tidytext)
 
-install.packages("tidytext")
-library(tidytext)
-
-set.seed(101)
+# Using Bag of words, from:
+# https://www.codementor.io/jadianes/data-science-python-r-sentiment-classification-machine-learning-du107otfg
 
 # Load SentiLex-PT base as data frame
 sentiWords <- readLines("~/Documents/Training-Set-MDM/SentiLex-lem-PT02.txt")
 sentiWords <- strsplit(sentiWords, "\\s*\\;")
-
 # Create columns from substrings
 colA <- c()
 colB <- c()
@@ -164,7 +154,6 @@ for(i in 1:length(sentiWords)) {
   colC <- c(colC, sentiWords[[i]][3])
   colD <- c(colD, sentiWords[[i]][4])
 }
-
 # names
 colA.1 <- colA
 colA <- gsub("\\.\\S+", "", colA)
@@ -174,14 +163,17 @@ colA.1 <- gsub("\\S+\\=", "", colA.1)
 colC <- gsub("\\S+\\=", "", colC)
 # Create data frame
 dadosSentiWords <- data.frame(name=colA, type=colA.1, pol=colC)
+colA.1 <- NULL; colA <- NULL; colB <- NULL; colC <- NULL; colD <- NULL
 # Remove NA row
 row_has_na <- apply(dadosSentiWords, 1, function(x){any(is.na(x))})
 dadosSentiWords <- dadosSentiWords[!row_has_na,]
+row_has_na <- NULL
+
+set.seed(1234)
 
 ### Shuffle data
-
 dadosLinear_matrix <- create_matrix(dadosLinear, language="portuguese", removeNumbers=TRUE,
-                             stemWords=TRUE, removeSparseTerms=.998, toLower = TRUE, removeStopwords = TRUE)
+                                    stemWords=TRUE, removeSparseTerms=.998, toLower = TRUE, removeStopwords = TRUE)
 
 
 palavras_freq = sort(rowSums(dadosLinear_matrix), decreasing = TRUE)
